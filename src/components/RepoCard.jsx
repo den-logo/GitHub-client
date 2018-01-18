e2d24ca1a69a4a69c35374cce2c8d75230384d56
den-logo <den.logunov@gmail.com>
2018-01-18 20:56:37 +0200

    add some font styles

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -9,13 +9,13 @@ export class RepoCard extends Component {
   }
   render() {
     return (
-      <li className="col-sm-4 repo-card">
+      <li className="col-4 repo-card">
         <div className="card">
           <div className="card-header text-center">
             <a href={this.props.url}>{ this.props.repoName }</a>
           </div>
           <div className="card-body">
-            { this.props.description }
+            <div class="rep-description">{ this.props.description }</div>
             { this.getHr() }
             <ul className="list-inline">
               <li className="list-inlie-item">{ this.props.language }</li>
@@ -23,7 +23,7 @@ export class RepoCard extends Component {
               <li className="list-inlie-item">{ this.props.fork }</li>
             </ul>
           </div>
-          <div className="card-footer"><i className="float-right">Updated on { this.props.updated }</i></div>
+          <div className="card-footer"><i className="float-right text-muted">Updated on { this.props.updated }</i></div>
         </div>
       </li>
     );
